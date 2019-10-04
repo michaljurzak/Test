c019d5f404428f3713bdb5a601c4cb5dd9dbdc2b
MichalJ <mjurzak7@gmail.com>
2019-10-04 13:44:41 +0200

    Number Wizard

--- test teoria/Assets/Scripts/HelloWorld.cs
+++ test teoria/Assets/Scripts/HelloWorld.cs
@@ -4,27 +4,48 @@ using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
+    double MileNaKilometr (double a)
+    {
+        a = a * 1.609344;
+        print(a);
+        return 0;
+    }
     // Start is called before the first frame update
+    //Białe znaki spacja, enter
     void Start()
     {
+        MileNaKilometr(14.5);
+        int min = 1;
+        int max = 1000;
         print("Hello World");
         print("Hello World");
         //zmienna to nazwana czesc pamienci komputera
         //RAM/ROM
         //RAM [][][][][]
-        string cokolwiek = "Hello World 02";//łancuch znakow
-        print("Hello World02");
+        //łancuch znakow
+        print("Witaj w Liczbowym Czrodzieju");
+        print("wybierz liczbe z przedzialu od 1 do 1000");
+        print("Maksymalna liczba jaka mozesz wybrac to" + max);
+        // + operator przeciazenia powoduje ze mozemy typy string
+        print("Minimalna liczba jaka mozesz wybrac to" + min);
+        //Metoda = funkcja krotki kawalek kodu ktory ma za zadanie wykonac jakies
+        // strrin / int/ float/ double/ long/ char
+        //string Ala ma kota
+        //int 1,2,3,4,5
+        print("Czy twoja liczba jest mniejsza lub wiekasza niz 500");
+        print("Nacisnij gorna strzlke aby odpowiedziec ze wieksza");
+        print("Nacisnij dolna strzlke aby odpowiedziec ze mniejsza");
+        print("Nacisnij enter aby potwierdzic");
+        if (Input.GetKeyDown(KeyCode.UpArrow)) //instrukcja warunkowa
+        {
 
-        print(cokolwiek);
-        int jakasnazwa = 5;
-        float jakasnazwa02 = 5.12222222222222222222;
-        double jakasnazwa03 = 5.122222222222222222222222222222222222;
 
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        print("Hello World 02");
+       
     }
 }
