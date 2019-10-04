227cce5bc220456014d00fd4493452f87adb4c24
Mateusz Duc <themrduc@gmail.com>
2019-10-04 12:32:16 +0200

    New unity project added

---
+++ test teoria/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello World");
+        print("Hello World");
+        //zmienna to nazwana czesc pamienci komputera
+        //RAM/ROM
+        //RAM [][][][][]
+        string cokolwiek = "Hello World 02";//łancuch znakow
+        print("Hello World02");
+
+        print(cokolwiek);
+        int jakasnazwa = 5;
+        float jakasnazwa02 = 5.12222222222222222222;
+        double jakasnazwa03 = 5.122222222222222222222222222222222222;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("Hello World 02");
+    }
+}
